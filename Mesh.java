a648605aa1490c72cecffe64021c653bf518b41b
Maksym Denysov <93475429+Javac-g@users.noreply.github.com>
2026-03-11 22:15:46 +0200

    Update Mesh.java

--- Mesh.java
+++ Mesh.java
@@ -1,9 +1,23 @@
+package com.max.hexopt.core.topology;
+
+import java.util.*;
+
 public class Mesh {
 
-    private final Map<Integer, Vertex> vertices = new HashMap<>();
-    private final List<HalfEdge> edges = new ArrayList<>();
-    private final List<Face> faces = new ArrayList<>();
+    private final Map<Integer, Vertex> vertices;
+    private final List<HalfEdge> edges;
+    private final List<Face> faces;
 
-    public Collection<Vertex> getVertices() { return vertices.values(); }
-    public List<Face> getFaces() { return faces; }
+    Mesh(Map<Integer, Vertex> vertices,
+         List<HalfEdge> edges,
+         List<Face> faces) {
+
+        this.vertices = Map.copyOf(vertices);
+        this.edges = List.copyOf(edges);
+        this.faces = List.copyOf(faces);
+    }
+
+    public Collection<Vertex> vertices() { return vertices.values(); }
+    public List<HalfEdge> edges() { return edges; }
+    public List<Face> faces() { return faces; }
 }
