8d2afa2ef129c3f6e05392fa546aa6bc140f4c8e
Maksym Denysov <93475429+Javac-g@users.noreply.github.com>
2026-03-11 22:14:59 +0200

    Update HalfEdge.java

--- HalfEdge.java
+++ HalfEdge.java
@@ -1,6 +1,19 @@
+package com.max.hexopt.core.topology;
+
 public class HalfEdge {
-    public Vertex origin;
-    public HalfEdge twin;
-    public HalfEdge next;
-    public Face face;
+
+    private Vertex origin;
+    private HalfEdge twin;
+    private HalfEdge next;
+    private Face face;
+
+    public Vertex origin() { return origin; }
+    public HalfEdge twin() { return twin; }
+    public HalfEdge next() { return next; }
+    public Face face() { return face; }
+
+    void setOrigin(Vertex origin) { this.origin = origin; }
+    void setTwin(HalfEdge twin) { this.twin = twin; }
+    void setNext(HalfEdge next) { this.next = next; }
+    void setFace(Face face) { this.face = face; }
 }
